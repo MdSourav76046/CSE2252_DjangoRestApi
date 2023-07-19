1373e7efde344be033158bbaeb6d01408baa0e98
MdSourav76046 <adibamama807@gmail.com>
2023-07-19 14:26:05 +0600

    new test added in unit test

--- DogProject/DogApp/tests.py
+++ DogProject/DogApp/tests.py
@@ -7,8 +7,8 @@ from .serializers import DogShopSerializer
 
 class post_dogShop_test(APITestCase):
     def setUp(self):
-        self.cat_url = reverse('dog_shop_list')
-        self.cat_data = {
+        self.dogShop_url = reverse('dog_shop_list')
+        self.dogShop_data = {
             "name": "Test Cat",
             "price": 2000.00,
             "breed": "american whiskey",
@@ -17,33 +17,34 @@ class post_dogShop_test(APITestCase):
         return super().setUp()
 
     def test_can_create_dogshop(self):
-        response = self.client.post(self.cat_url, self.cat_data)
+        response = self.client.post(self.dogShop_url, self.dogShop_data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
 class get_dogShop_list(APITestCase):
     def setUp(self):
-        self.cat_url = reverse('dog_shop_list')
+        self.dogShop_url = reverse('dog_shop_list')
         return super().setUp()
 
     def test_can_get_dogshop(self):
-        response = self.client.get(self.cat_url)
+        response = self.client.get(self.dogShop_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-# class get_cat_detail(APITestCase):
-#     def setUp(self):
-#         self.cat_data = {
-#             "name": "Test Cat",
-#             "price": 1000.00,
-#             "description": "Test Discription",
-#         }
-#         self.response = self.client.post(reverse('product_view'), self.cat_data)
-#         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
-#         self.cat_url = reverse('product_details', kwargs={'pk': 1})
-#         return super().setUp()
+class get_dogShop_detail(APITestCase):
+    def setUp(self):
+        self.dogShop_data = {
+            "name": "Test Dog",
+            "price": 1000.00,
+            "breed": "Indian dog",
+            "description": "Highly ricky",
+        }
+        self.response = self.client.post(reverse('dog_shop_list'), self.dogShop_data)
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+        self.dogShop_url = reverse('dog_shop_details', kwargs={'pk': 1})
+        return super().setUp()
 
-#     def test_can_get_cat(self):
-#         response = self.client.get(self.cat_url)
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_can_get_dogShop(self):
+        response = self.client.get(self.dogShop_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
 # class put_cat_test(APITestCase):
