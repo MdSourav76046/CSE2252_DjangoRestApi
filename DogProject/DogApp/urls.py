ba8bd9b613e495fb574880ffcec75ee3586a21ef
MdSourav76046 <adibamama807@gmail.com>
2023-07-19 13:56:48 +0600

    update views.py

--- DogProject/DogApp/urls.py
+++ DogProject/DogApp/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import Do
+from .views import DogShopDetail
 urlpatterns = [
     path('admin/',),
 ]
--- DogProject/DogApp/views.py
+++ DogProject/DogApp/views.py
@@ -3,6 +3,9 @@ from rest_framework import generics
 from .models import DogShop
 from .serializers import DogShopSerializer
 
+class DogShopList(generics.ListCreateAPIView):
+    queryset = DogShop.objects.all()
+    serializer_class = DogShopSerializer
 
 class DogShopDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = DogShop.objects.all()
