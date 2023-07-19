4b1eab390cc3319dbc2581101efb4087f15f057e
MdSourav76046 <adibamama807@gmail.com>
2023-07-19 13:53:34 +0600

    added views.py

--- DogProject/DogApp/serializers.py
+++ DogProject/DogApp/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from .models import DogShop
 
 
-class BreedSerializer(serializers.ModelSerializer):
+class DogShopSerializer(serializers.ModelSerializer):
     class Meta:
         model = DogShop
         fields = '__all__'
--- DogProject/DogApp/views.py
+++ DogProject/DogApp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import DogShop
+from .serializers import DogShopSerializer
 
-# Create your views here.
+
+class DogList(generics.ListCreateAPIView):
+    queryset = DogShop.objects.all()
+    serializer_class = DogShopSerializer
+
+
+class DogDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = DogShop.objects.all()
+    serializer_class = DogShopSerializer
+
