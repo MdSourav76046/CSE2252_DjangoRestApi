d574fc2d857efcd5a4147e84f67c124b7e8a2c04
MdSourav76046 <adibamama807@gmail.com>
2023-07-19 14:29:48 +0600

    more test case added in tests.py

--- DogProject/DogApp/tests.py
+++ DogProject/DogApp/tests.py
@@ -47,40 +47,43 @@ class get_dogShop_detail(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
-# class put_cat_test(APITestCase):
-#     def setUp(self):
-#         self.cat_data = {
-#             "name": "Test Cat",
-#             "price": 1000.00,
-#             "description": "Test Discription",
-#         }
-#         self.response = self.client.post(reverse('product_view'), self.cat_data)
-#         print(self.response.data)
-#         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
-#         self.cat_url = reverse('product_details', kwargs={'pk': 1})
-#         return super().setUp()
+class put_dogShop_test(APITestCase):
+    def setUp(self):
+        self.dogShop_data = {
+            "name": "Test Cat",
+            "price": 1000.00,
+            "breed": "Indian dog",
+            "description": "Test Discription",
+        }
+        self.response = self.client.post(reverse('dog_shop_list'), self.dogShop_data)
+        print(self.response.data)
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+        self.dogShop_url = reverse('dog_shop_details', kwargs={'pk': 1})
+        return super().setUp()
 
-#     def test_can_put_cat(self):
-#         self.cat_data = {
-#             "name": "Test Cat2",
-#             "price": 1000.00,
-#             "description": "Test Discription2",
-#         }
-#         response = self.client.put(self.cat_url, self.cat_data)
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_can_put_dogShop(self):
+        self.dogShop_data = {
+            "name": "Test Cat2",
+            "price": 1000.00,
+            "breed": "Indian dog",
+            "description": "Test Discription2",
+        }
+        response = self.client.put(self.dogShop_url, self.dogShop_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-# class delete_cat(APITestCase):
-#     def setUp(self):
-#         self.cat_data = {
-#             "name": "Test Cat",
-#             "price": 1000.00,
-#             "description": "Test Discription",
-#         }
-#         self.response = self.client.post(reverse('product_view'), self.cat_data)
-#         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
-#         self.cat_url = reverse('product_details', kwargs={'pk': 1})
-#         return super().setUp()
+class delete_dogShop(APITestCase):
+    def setUp(self):
+        self.dogShop_data = {
+            "name": "Test Cat",
+            "price": 1000.00,
+            "breed": "Indian dog",
+            "description": "Test Discription",
+        }
+        self.response = self.client.post(reverse('dog_shop_list'), self.dogShop_data)
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+        self.dogShop_url = reverse('dog_shop_details', kwargs={'pk': 1})
+        return super().setUp()
 
-#     def test_can_delete_cat(self):
-#         response = self.client.delete(self.cat_url)
-#         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+    def test_can_delete_dogShop(self):
+        response = self.client.delete(self.dogShop_url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
